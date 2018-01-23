3081c6e8510f896514f32390f1758e6a5addd14a
Guest User <Guest@epicodus-7.local>
2018-01-23 09:30:00 -0800

    add pipe to cooking component

--- src/app/cooking/cooking.component.ts
+++ src/app/cooking/cooking.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Post } from '../post.model';
+import { PostService } from '../post.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cooking',
   templateUrl: './cooking.component.html',
-  styleUrls: ['./cooking.component.css']
+  styleUrls: ['./cooking.component.css'],
+  providers: [PostService]
 })
-export class CookingComponent {
+export class CookingComponent implements OnInit {
+  // linter requests to remove type 'string', will not work without declaration
+  filterByCategory: string = 'cookingPost';
+  posts: Post[];
 
+  constructor(private router: Router, private postService: PostService) {}
 
+  ngOnInit() {
+    this.posts = this.postService.getPosts();
+  }
+
+  goToDetailPage(clickedPost: Post) {
+    this.router.navigate(['posts', clickedPost.id]);
+  }
 
 }
